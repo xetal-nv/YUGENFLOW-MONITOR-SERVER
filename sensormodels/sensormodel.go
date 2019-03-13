2f6f87896a801a0f7959675ec71204b9855bd9e8
Francesco Pessolano <fpessolano@users.noreply.github.com>
2019-03-13 15:07:23 +0100

    new TCP server/protocol Added sensor model adding entry counting algo - to be tested in real office adding API server -in progress (reporting, command API)

--- sensormodels/sensormodel.go
+++ sensormodels/sensormodel.go
@@ -9,38 +9,67 @@ import (
 	"time"
 )
 
-// TODO make full sensor model with comands as well
-func SensorModel(id, iter, mxdel int, vals []int) {
+var command = map[byte][]byte{
+	[]byte("\x07")[0]: []byte("\x01\x02"),
+	[]byte("\x09")[0]: []byte("\x01\x02"),
+	[]byte("\x0b")[0]: []byte("\x01\x02"),
+	[]byte("\x0d")[0]: []byte("\x01\x02"),
+}
+
+// TODO to be verified
+func SensorModel(id, iter, mxdelay int, vals []int) {
 
 	time.Sleep(2 * time.Second)
 	fmt.Println(" TEST -> Connect to TCP channel")
 	port := os.Getenv("TCPPORT")
 	conn, e := net.Dial(os.Getenv("TCPPROT"), "0.0.0.0:"+port)
+	//noinspection GoUnhandledErrorResult
+	defer conn.Close()
 	if e != nil {
 		fmt.Println("Unable to connect")
 	} else {
+		// sensor registers first
 		//noinspection GoUnhandledErrorResult
 		conn.Write([]byte{'a', 'b', 'c', 1, 2, 3})
+		// start a listener
+		c := make(chan []byte, 5)
+		go func(c chan []byte) {
+			var e error
+			for e == nil {
+				cmd := make([]byte, 1)
+				if _, e = conn.Read(cmd); e == nil {
+					fmt.Printf("Sensor %v has received data %v\n", id, cmd)
+					c <- cmd
+				}
+			}
+		}(c)
 		for i := 0; i < iter; i++ {
+			// sensor model loop starts with seding a data element
 			rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
 			data := vals[rand.Intn(len(vals))]
-			del := rand.Intn(mxdel) + 1
+			del := rand.Intn(mxdelay) + 1
 			//noinspection GoUnhandledErrorResult
 			msg := []byte{1, 0, byte(id), byte(data)}
 			msg = append(msg, codings.Crc8(msg))
 			_, _ = conn.Write(msg)
-			time.Sleep(time.Duration(del) * 1000 * time.Millisecond)
-			if i == 5 {
-				msg := []byte{2}
-				msg = append(msg, codings.Crc8(msg))
-				fmt.Println("sending something else", msg)
-				_, _ = conn.Write(msg)
-				time.Sleep(time.Duration(del) * 1000 * time.Millisecond)
+			// fork for either sending a new data value or receiving a command
+			select {
+			case v := <-c:
+				crc := codings.Crc8(v[:len(v)-1])
+				if crc == v[len(v)-1] {
+					msg := []byte{v[0]}
+					if rt, ok := command[v[0]]; ok {
+						msg = append(msg, rt...)
+					}
+					crc = codings.Crc8(msg)
+					msg = append(msg, crc)
+					_, e = conn.Write(msg)
+				}
+			case <-time.After(time.Duration(del) * 1000 * time.Millisecond):
+				// continue to send data
 			}
-
 		}
 	}
-	//noinspection GoUnhandledErrorResult
-	conn.Close()
-	fmt.Println(" TEST -> Disconnect to TCP channel")
+
+	fmt.Printf("Sensor %v disconnecting\n", id)
 }
