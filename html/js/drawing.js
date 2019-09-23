b8746d798981152e875934f2c2124f78d3713832
Francesco Pessolano <fpessolano@users.noreply.github.com>
2019-09-23 11:20:56 +0200

    target 0.8.0: removed JS UI bug on flows target 0.8.0: added office SVG target 0.8.0: added possibility to merge serves in one port

--- html/js/drawing.js
+++ html/js/drawing.js
@@ -27,7 +27,8 @@ let dataArraysFlow = [];
 // let dataPoints = [];
 let mapnames = {};
 
-var ip = "http://" + location.hostname + ":" + port;
+var ip = "http://" + location.hostname + ":" + location.port;
+console.log(ip);
 
 const colors = [
     '#000000', '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
