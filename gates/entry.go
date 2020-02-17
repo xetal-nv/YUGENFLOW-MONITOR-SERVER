93366887c736fdbfa524097a7e5bcff3c0bef061
Francesco Pessolano <fpessolano@users.noreply.github.com>
2020-02-17 14:27:15 +0100

    target 0.7.2: added automatic deactivation of a defected sensor

--- gates/entry.go
+++ gates/entry.go
@@ -10,13 +10,16 @@ import (
 	"sync"
 )
 
+// NOTE this is build for one or two sensors gates. Part of the code can support more, but the algorithm not
+
 // set-up for the processing of sensor/gate data into flow values for the associated entry id
 // new sensor data is passed by means of the in Channel snd send to the proper space via a spaces.SendData call
 func entryProcessingSetUp(id int, in chan sensorData, entryList EntryDef) {
 	var scratchPad scratchData
 	sensorListEntry := make(map[int]sensorData)
 	sensorToGate := make(map[int]int)
-	sensorSensorDifferential := make(map[int][]int)
+	sensorDifferential := make(map[int][]int)
+	sensorDifferentialTimes := make(map[int][]int)
 	//gateListEntry := entryList.Gates
 
 	scratchPad.senData = make(map[int]sensorData)
@@ -36,18 +39,26 @@ func entryProcessingSetUp(id int, in chan sensorData, entryList EntryDef) {
 	for ind, el := range entryList.Gates {
 		for _, sen := range el {
 			sensorToGate[sen] = ind
-			sensorSensorDifferential[ind] = append(sensorSensorDifferential[ind], 0)
+			sensorDifferential[ind] = append(sensorDifferential[ind], 0)
+			sensorDifferentialTimes[ind] = append(sensorDifferentialTimes[ind], 0)
 		}
 	}
 
-	entryProcessingCore(id, in, sensorListEntry, entryList.Gates, scratchPad, sensorToGate, sensorSensorDifferential)
+	//fmt.Println(id)
+	//fmt.Println(sensorListEntry)
+	//fmt.Println(sensorToGate)
+	//fmt.Println(sensorDifferential)
+	//fmt.Println(sensorDifferentialTimes)
+	//os.Exit(1)
+
+	entryProcessingCore(id, in, sensorListEntry, entryList.Gates, scratchPad, sensorToGate, sensorDifferential, sensorDifferentialTimes)
 
 }
 
 // implements the core logic od the sensor/gate data processing
 // it also checks if a gate sensor is more active than another one and initiate reset of all sensors in the gate when needed
-func entryProcessingCore(id int, in chan sensorData, sensorListEntry map[int]sensorData,
-	gateListEntry map[int][]int, scratchPad scratchData, sensorToGate map[int]int, sensorSensorDifferential map[int][]int) {
+func entryProcessingCore(id int, in chan sensorData, sensorListEntry map[int]sensorData, gateListEntry map[int][]int, scratchPad scratchData, sensorToGate map[int]int,
+	sensorDifferential map[int][]int, sensorDifferentialTimes map[int][]int) {
 	var f *os.File
 	var err error
 	var tryResetMux sync.RWMutex
@@ -57,6 +68,7 @@ func entryProcessingCore(id int, in chan sensorData, sensorListEntry map[int]sen
 	}
 
 	//fmt.Println(sensorListEntry)
+	//fmt.Println(id, gateListEntry[sensorToGate[id]])
 	//os.Exit(1)
 
 	for senId := range sensorToGate {
@@ -72,13 +84,15 @@ func entryProcessingCore(id int, in chan sensorData, sensorListEntry map[int]sen
 				_ = f.Close()
 			}
 			log.Printf("Gates.entryProcessingCore: recovering for entry %v due to %v\n ", id, e)
-			go entryProcessingCore(id, in, sensorListEntry, gateListEntry, scratchPad, sensorToGate, sensorSensorDifferential)
+			go entryProcessingCore(id, in, sensorListEntry, gateListEntry, scratchPad, sensorToGate, sensorDifferential, sensorDifferentialTimes)
 		}
 	}()
 	log.Printf("Gates.entry: Processing: setting entry %v\n", id)
 	for {
 		data := <-in
 		nv := data.val
+		// add here the control on how many it went asymmetric
+		//fmt.Println(gateListEntry[sensorToGate[data.id]])
 
 		// check for asymmetry in gate sensor dictating reset if there is more than one device in the gate
 		if maximumAsymmetry != 0 && len(gateListEntry[sensorToGate[data.id]]) > 1 {
@@ -88,47 +102,74 @@ func entryProcessingCore(id int, in chan sensorData, sensorListEntry map[int]sen
 					tryResetMux.RLock()
 					if tryReset[r] {
 						// this array does not need locking, races are not possible despite the go routine on it
-						sensorSensorDifferential[sensorToGate[r]][i] += 1
+						sensorDifferential[sensorToGate[r]][i] += 1
 						tryResetMux.RUnlock()
-						var min, max int
-						for i, e := range sensorSensorDifferential[sensorToGate[r]] {
+						var min, max, index int
+						for i, e := range sensorDifferential[sensorToGate[r]] {
 							if i == 0 || e < min {
 								min = e
+								index = i
 							}
 						}
-						for i, e := range sensorSensorDifferential[sensorToGate[r]] {
+						for i, e := range sensorDifferential[sensorToGate[r]] {
 							if i == 0 || e > max {
 								max = e
 							}
 							tryResetMux.Lock() // this locking is redundant but still required by the compiler
-							sensorSensorDifferential[sensorToGate[r]][i] -= min
+							sensorDifferential[sensorToGate[r]][i] -= min
 							tryResetMux.Unlock() // this locking is redundant but still required by the compiler
 						}
 						if max-min >= maximumAsymmetry {
+							sensorDifferentialTimes[sensorToGate[r]][index] += 1
 							tryResetMux.Lock() // this locking is redundant but still required by the compiler
 							tryReset[r] = false
 							tryResetMux.Unlock() // this locking is redundant but still required by the compiler
-							go func(ids []int) {
-								for _, deviceId := range ids {
-									// we do not wait for a success as failure to execute will eventually result in another reset request
-									SensorRst.RLock()
-									resetChannel, ok := SensorRst.Channel[deviceId]
-									SensorRst.RUnlock()
-									if ok {
-										resetChannel <- true
-										log.Println("sent asymmetric reset request for device", deviceId)
-									} else {
-										log.Printf("cannot reset device %v since not connected\n", deviceId)
+							//fmt.Println("sensorDifferentialTimes", sensorDifferentialTimes[sensorToGate[data.id]], gateListEntry[sensorToGate[data.id]][index])
+							if sensorDifferentialTimes[sensorToGate[data.id]][index] < maximumAsymmetryIter {
+								go func(ids []int) {
+									for _, deviceId := range ids {
+										// we do not wait for a success as failure to execute will eventually result in another reset request
+										SensorRst.RLock()
+										resetChannel, ok := SensorRst.Channel[deviceId]
+										SensorRst.RUnlock()
+										if ok {
+											resetChannel <- true
+											log.Println("sent asymmetric reset request for device", deviceId)
+										} else {
+											log.Printf("cannot reset device %v since not connected\n", deviceId)
+										}
+									}
+									tryResetMux.Lock()
+									for i := range sensorDifferential[sensorToGate[id]] {
+										sensorDifferential[sensorToGate[id]][i] = 0
+									}
+									// for safety of races we should use a RW lock on tryReset
+									tryReset[r] = true
+									tryResetMux.Unlock()
+								}(gateListEntry[sensorToGate[data.id]])
+							} else {
+								log.Printf("Sensor %v of gate %v has been disabled due to exceding limit on asymmetric reset\n",
+									gateListEntry[sensorToGate[data.id]][index], sensorToGate[data.id])
+								//fmt.Printf("Sensor %v of gate %v has been disabled due to exceding limit on asymmetric reset\n",
+								//	gateListEntry[sensorToGate[data.id]][index], sensorToGate[data.id])
+								var tmp, tmpDiff, tmpTimes []int
+								//fmt.Println(gateListEntry[sensorToGate[data.id]])
+								//fmt.Println(sensorDifferential[sensorToGate[data.id]])
+								//fmt.Println(sensorDifferentialTimes[sensorToGate[data.id]])
+								for i, val := range gateListEntry[sensorToGate[data.id]] {
+									if i != index {
+										tmp = append(tmp, val)
+										tmpDiff = append(tmpDiff, 0)
+										tmpTimes = append(tmpTimes, 0)
 									}
 								}
-								tryResetMux.Lock()
-								for i := range sensorSensorDifferential[sensorToGate[id]] {
-									sensorSensorDifferential[sensorToGate[id]][i] = 0
-								}
-								// for safety of races we should use a RW lock on tryReset
-								tryReset[r] = true
-								tryResetMux.Unlock()
-							}(gateListEntry[sensorToGate[data.id]])
+								gateListEntry[sensorToGate[data.id]] = tmp
+								sensorDifferential[sensorToGate[data.id]] = tmpDiff
+								sensorDifferentialTimes[sensorToGate[data.id]] = tmpTimes
+								//fmt.Println(gateListEntry[sensorToGate[data.id]])
+								//fmt.Println(sensorDifferential[sensorToGate[data.id]])
+								//fmt.Println(sensorDifferentialTimes[sensorToGate[data.id]])
+							}
 						}
 					} else {
 						tryResetMux.RUnlock()
@@ -208,6 +249,7 @@ func trackPeople(id int, sensorListEntry map[int]sensorData, gateListEntry map[i
 
 	for _, gate := range gateListEntry {
 		if len(gate) == 1 {
+			//fmt.Println("single device", gate)
 			// in case of single device the data is passed as it
 			rt = scratchPad.senData[gate[0]].val
 			scratchPad.unusedSampleSumIn[gate[0]] = 0
--- gates/init.go
+++ gates/init.go
@@ -21,7 +21,7 @@ func SetUp() {
 	if v, e := strconv.Atoi(strings.Trim(os.Getenv("RESETPERIOD"), " ")); e == nil {
 		if v != 0 {
 			if da := os.Getenv("DEVICEASYM"); da == "" {
-				maximumAsymmetry = 10
+				maximumAsymmetry = 5
 			} else {
 				if v, e := strconv.Atoi(da); e != nil {
 					log.Fatal("spaces.SetUp: fatal error in definition of DEVICEASYM")
@@ -34,8 +34,17 @@ func SetUp() {
 	if maximumAsymmetry == 0 {
 		log.Printf("!!! WARNING GATE ASYMMETRY RESET IS NOT ENABLED !!!\n")
 	} else {
+		if da := os.Getenv("DEVICEASYMCMAX"); da == "" {
+			maximumAsymmetryIter = 4
+		} else {
+			if v, e := strconv.Atoi(da); e != nil {
+				log.Fatal("spaces.SetUp: fatal error in definition of DEVICEASYMCMAX")
+			} else {
+				maximumAsymmetryIter = v
+			}
+		}
 		log.Printf("!!! WARNING GATE ASYMMETRY RESET IS ENABLED !!!\n")
-		log.Printf("spaces.SetUp: setting maximum asymmetry for gate sensors at %v\n", maximumAsymmetry)
+		log.Printf("spaces.SetUp: setting maximum asymmetry for gate sensors at %v with maximum iters at %v\n", maximumAsymmetry, maximumAsymmetryIter)
 	}
 
 	if data := os.Getenv("REVERSE"); data != "" {
