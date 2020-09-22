0b564418a2f1f525a26f264034e4f9512593a5cb
Francesco Pessolano <fpessolano@users.noreply.github.com>
2020-09-22 21:30:21 +0200

    see changelog file

---
+++ dbs/sensorDB/start.go
@@ -0,0 +1,80 @@
+package sensorDB
+
+import (
+	"errors"
+	"fmt"
+	"gateserver/support/globals"
+	"github.com/fpessolano/mlogger"
+	bolt "go.etcd.io/bbolt"
+	"os"
+	"path/filepath"
+)
+
+var main *bolt.DB
+
+const (
+	definitions   = "definitions"
+	activeDevices = "activeDevices"
+	maliciousMac  = "maliciousMac"
+	maliciousIp   = "maliciousIp"
+)
+
+func Start() {
+	var err error
+	if globals.SensorDBLog, err = mlogger.DeclareLog("yugenflow_gsensorDB", false); err != nil {
+		fmt.Println("Fatal Error: Unable to set yugenflow_gsensorDB logfile.")
+		os.Exit(0)
+	}
+	if err = mlogger.SetTextLimit(globals.SensorDBLog, 80, 20, 10); err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	if err = os.MkdirAll(globals.DiskCachePath, os.ModePerm); err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	if main, err = bolt.Open(filepath.Join(globals.DiskCachePath, "devicetable.db"), 0600, nil); err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	//noinspection GoNilness
+	if err := main.Update(func(tx *bolt.Tx) error {
+		_, err = tx.CreateBucketIfNotExists([]byte(definitions))
+		if err != nil {
+			return errors.New("could not create " + definitions + " bucket: " + err.Error())
+		}
+		_, err = tx.CreateBucketIfNotExists([]byte(activeDevices))
+		if err != nil {
+			return errors.New("could not create " + activeDevices + " bucket: " + err.Error())
+		}
+		_, err = tx.CreateBucketIfNotExists([]byte(maliciousMac))
+		if err != nil {
+			return errors.New("could not create " + maliciousMac + " bucket: " + err.Error())
+		}
+		_, err = tx.CreateBucketIfNotExists([]byte(maliciousIp))
+		if err != nil {
+			return errors.New("could not create " + maliciousIp + " bucket: " + err.Error())
+		}
+		return nil
+	}); err != nil {
+		mlogger.Panic(globals.SensorDBLog,
+			mlogger.LoggerData{"sensorDB.Start", "Error in opening buckets: " + err.Error(),
+				[]int{}, false}, true)
+	}
+	mlogger.Info(globals.SensorDBLog,
+		mlogger.LoggerData{"sensorDB.Start", "service started",
+			[]int{1}, true})
+
+	fmt.Println("SensorDB initialised")
+}
+
+func Close() {
+	_ = main.Close()
+	fmt.Println("SensorDB closed")
+	mlogger.Info(globals.SensorDBLog,
+		mlogger.LoggerData{"sensorDB.Start", "service stopped",
+			[]int{1}, true})
+}
